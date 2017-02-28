1c4d1a1434ed8707353c20c73a16680ff2dbdaec
Yizhou Wang <wangyizhou3@gmail.com>
2017-02-27 16:56:13 -0800

    configure version in cmake

---
+++ include/Config.h
@@ -0,0 +1,3 @@
+// the configured options for cmake
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
--- src/hello_world.cc
+++ src/hello_world.cc
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <Config.h>
 
 int main(int argc, char** argv)
 {
+    std::cout << "Version Major: " << VERSION_MAJOR << ", Version Minor: " << VERSION_MINOR << std::endl;
     std::cout << "Hello world!" << std::endl;
     return 0;
 
